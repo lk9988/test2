9579871b9fa24b4a63e7a32e65b338791a0497dd
lana <lanabkim@gmail.com>
2020-12-25 21:17:51 -0800

    adding new stuff

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const PORT = process.env.PORT || 4321;
 app.get("/", function (req, res) {
 	res.send("hello world");
 });
+app.get("/test", function (req, res) {
+	res.send("hello world test ! ");
+});
 
 // app.listen(PORT);
 app.listen(PORT, function () {
